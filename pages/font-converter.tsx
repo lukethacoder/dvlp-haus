14be69209f35d91ab7902fb11b3af8be9b762790
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2019-06-16 14:49:56 +1000

    chaning font conversion calculation - no 100% yet

--- pages/font-converter.tsx
+++ pages/font-converter.tsx
@@ -4,140 +4,106 @@ import Clipboard from 'react-clipboard.js';
 import Layout from '../components/Layout'
 import Container from '../components/Container'
 
+interface InputVal {
+  name: string,
+  raw: string,
+  value: number,
+  ext?: string
+}
+
 const SvgToCssPage: React.FunctionComponent = () => {
-  const [pixels, setPixels] = React.useReducer(
+  const [allInputs, setAllInputs] = React.useReducer(
     (state, newState) => ({...state, ...newState}),
     {
-      name: 'px',
-      raw_input: '',
-      value: 16,
+      pixels: {
+        name: 'pixels',
+        raw: "16px",
+        value: 16,
+      },      
+      rem: {
+        name: 'rem',
+        raw: "1rem",
+        value: 1
+      },      
+      em: {
+        name: 'em',
+        raw: "1em",
+        value: 1
+      },      
+      custom: {
+        name: 'custom',
+        raw: "1dvlp",
+        value: 1,
+        ext: 'dvlp'
+      },
+      base: {
+        name: 'base',
+        raw: '16px',
+        value: 16
+      }
     }
   )
-  const [rem, setRem] = React.useReducer(
-    (state, newState) => ({...state, ...newState}),
-    {
-      name: 'rem',
-      raw_input: '',
-      value: 1,
-    }
-  )
-  const [em, setEm] = React.useReducer(
-    (state, newState) => ({...state, ...newState}),
-    {
-      name: 'em',
-      raw_input: '',
-      value: 1,
-    }
-  )
-  const [custom, setCustom] = React.useReducer(
-    (state, newState) => ({...state, ...newState}),
-    {
-      name: 'custom',
-      ext: 'dvlp',
-      raw_input: '',
-      value: 1,
-    }
-  )
-  const [baseFont, setBaseFont] = React.useReducer(
-    (state, newState) => ({...state, ...newState}),
-    {
-      name: 'base',
-      raw_input: '16',
-      value: 16,
-    }
-  )
-  // const [remVal, setRemVal] = React.useState('');
-  // const [baseFontSize, setBaseFontSize] = React.useState(16);
-  // const [customExt, setCustomExt] = React.useState('dvlp');
+
+  /*
+    key - input name
+    raw - input value from <input>
+    value - computed value (number)
+  */
+  const updateInput = (key: string, raw: any, value: any) => {
+    console.warn('key => ', key, 'raw => ', raw, 'value => ', value)
+    setAllInputs({
+      ...allInputs,
+      [key]: {
+        value: value,
+        raw: raw,
+        ...allInputs[key],
+      }
+    })
+  }
+
+  function changeInput(event: any) {
+    console.log('name => ', event.target.name, "value => ", event.target.value)
+    updateInput(
+      event.target.name,
+      event.target.value,
+      (parseFloat(event.target.value) ? parseFloat(event.target.value) : 0)
+    );
+  }
 
   async function handleInputChange(event: any) {
-    console.log(event.target.name)
-    switch (event.target.name){
-      case 'px': 
-        setPixels({
-          ...pixels,
-          value: (event.target.value !== '' ? parseFloat(event.target.value) ? parseFloat(event.target.value) : pixels.value : ''),
-          raw_input: event.target.value,
-        })
-        setRem({
-          ...rem,
-          raw_input: (event.target.value !== '' ? parseFloat(event.target.value) ? `${(parseFloat(event.target.value) / baseFont.value)}rem` : rem.value : ''),
-        })
-        setEm({
-          ...em,
-          raw_input: (event.target.value !== '' ? parseFloat(event.target.value) ? `${(parseFloat(event.target.value) / baseFont.value)}em` : em.value : ''),
-        })
-        setCustom({
-          ...custom,
-          raw_input: (event.target.value !== '' ? parseFloat(event.target.value) ? `${(parseFloat(event.target.value) / baseFont.value)}custom` : custom.value : ''),
-        })
-        break;
-      case 'rem' || 'em' || 'custom':
-        const value = event.target.value;
-        switch (event.target.name) {
-          case 'rem':
-            setRem({
-              ...rem,
-              value: (value !== '' ? parseFloat(value) ? parseFloat(value) : rem.value : ''),
-              raw_input: value,
-            })
-            setEm({
-              ...em,
-              raw_input: (value !== '' ? parseFloat(value) ? `${parseFloat(value)}em` : `${em.value}em` : ''),
-            })
-            setCustom({
-              ...custom,
-              raw_input: (value !== '' ? parseFloat(value) ? `${parseFloat(value)}custom` : custom.value : ''),
-            })
-            break;
-          case 'em':
-            setEm({
-              ...em,
-              value: (value !== '' ? parseFloat(value) ? parseFloat(value) : em.value : ''),
-              raw_input: value,
-            })
-            setRem({
-              ...rem,
-              raw_input: (value !== '' ? parseFloat(value) ? `${parseFloat(value)}rem` : rem.value : '')
-            })
-            setCustom({
-              ...custom,
-              raw_input: (value !== '' ? parseFloat(value) ? `${parseFloat(value)}custom` : custom.value : ''),
-            })
-            break;
-          case 'custom':
-            setCustom({
-              ...custom,
-              value: (value !== '' ? parseFloat(value) ? parseFloat(value) : custom.value : ''),
-              raw_input: value,
-            })
-            break;
+    let name = event.target.name;
+    let value = event.target.value;
+    console.log('name => ', event.target.name, "value => ", event.target.value)
+    
+    Object.keys(allInputs).map((key: string) => {
+      // update field with users input if currently focused field
+      if (allInputs[key].name === name) {
+        console.log('does equal', key)
+        console.warn('value => ', value, 'key => ', key)
+        // updateInput(allInputs[key].name, event.target.value, (parseFloat(event.target.value) ? parseFloat(event.target.value) : 0));
+      } else {
+        console.log('not allInputs.base.value => ', (allInputs.base.value));
+        // calculate other values based on users input val
+        if (allInputs[key].name === 'pixels') {
+          console.log('does equal', key)
         }
-        setPixels({
-          ...pixels,
-          raw_input: (value !== '' ? parseFloat(value) ? `${(parseFloat(value) * baseFont.value)}px` : pixels.value : ''),
-        })
-        break;
-      case 'baseFont': 
-        setBaseFont({
-          ...baseFont,
-          value: (event.target.value !== '' ? parseFloat(event.target.value) ? parseFloat(event.target.value) : rem.value : ''),
-          raw_input: event.target.value,
-        })
-        setPixels({
-          ...pixels,
-          raw_input: (value !== '' ? parseFloat(value) ? `${(parseFloat(value) * event.target.value)}px` : pixels.value : '')
-        })
-        setRem({
-          ...rem,
-          raw_input: (rem.value !== '' ? parseFloat(rem.value) ? `${parseFloat(rem.value) / parseFloat(event.target.value)}rem` : rem.value : ''),
-        })
-        setEm({
-          ...em,
-          raw_input: (rem.value !== '' ? parseFloat(em.value) ? `${parseFloat(em.value) / parseFloat(event.target.value) }em`: em.value : ''),
-        })
-        break;
-    }
+        else if (allInputs[key].name === 'rem') {
+          console.table({
+            base: allInputs.base.value,
+            name: allInputs[key].name,
+            parse: parseFloat(event.target.value),
+            computed: (parseFloat(event.target.value) / allInputs.base.value),
+          })
+          // updateInput(
+          //   'rem',
+          //   'test',
+          //   (value !== '' ? parseFloat(value) ? `${(parseFloat(value) / allInputs.base.value)}rem` : allInputs.rem.value : ''),
+          // )
+        }
+        
+        // updateInput(allInputs[key].name, allInputs[key].raw, value);
+      }
+    })
   }
 
   return (
@@ -146,70 +112,73 @@ const SvgToCssPage: React.FunctionComponent = () => {
         <div className="font__converter grid grid-2">
           <div className="input_field">
             <div className="btn-settings">
-              <Clipboard className="clipboard__btn" data-clipboard-text={`${pixels.raw_input}`}>
+              <Clipboard className="clipboard__btn" data-clipboard-text={`${allInputs.pixels.raw}`}>
                 Copy
               </Clipboard>
             </div>
             <div className="input_field_container">
               <label>PX</label>
               <input
-                value={`${pixels.raw_input}`}
-                name="px"
+                value={`${allInputs.pixels.raw}`}
+                name="pixels"
                 type="text"
                 placeholder="16px"
-                onChange={handleInputChange}
+                onKeyUp={handleInputChange}
+                onChange={changeInput}
               />
             </div>
           </div>
           <div className="input_field">
             <div className="btn-settings">
-              <Clipboard className="clipboard__btn" data-clipboard-text={`${em.raw_input}`}>
+              <Clipboard className="clipboard__btn" data-clipboard-text={`${allInputs.em.raw}`}>
                 Copy
               </Clipboard>
             </div>
             <div className="input_field_container">
               <label>Em</label>
               <input
-                value={`${em.raw_input}`}
+                value={`${allInputs.em.raw}`}
                 name="em"
                 type="text"
                 placeholder="1em"
-                onChange={handleInputChange}
+                onKeyPress={handleInputChange}
+                onChange={changeInput}
               />
             </div>
           </div>
 
           <div className="input_field">
             <div className="btn-settings">
-              <Clipboard className="clipboard__btn" data-clipboard-text={`${rem.raw_input}`}>
+              <Clipboard className="clipboard__btn" data-clipboard-text={`${allInputs.rem.raw}`}>
                 Copy
               </Clipboard>
             </div>
             <div className="input_field_container">
               <label>Rem</label>
               <input
-                value={`${rem.raw_input}`}
+                value={`${allInputs.rem.raw}`}
                 name="rem"
                 type="text"
                 placeholder="1rem"
-                onChange={handleInputChange}
+                onKeyPress={handleInputChange}
+                onChange={changeInput}
               />
             </div>
           </div>
           <div className="input_field">
             <div className="btn-settings">
-              <Clipboard className="clipboard__btn" data-clipboard-text={`${custom.raw_input}`}>
+              <Clipboard className="clipboard__btn" data-clipboard-text={`${allInputs.custom.raw}`}>
                 Copy
               </Clipboard>
             </div>
             <div className="input_field_container">
               <label>Custom</label>
               <input
-                value={`${custom.raw_input}`}
+                value={`${allInputs.custom.raw}`}
                 name="custom"
                 type="text"
-                placeholder={`1${custom.customExt ? custom.customExt : 'dvlp'}`}
-                onChange={handleInputChange}
+                placeholder={`1${allInputs.custom.ext ? allInputs.custom.ext : 'dvlp'}`}
+                onChange={changeInput}
               />
             </div>
           </div>
@@ -218,11 +187,11 @@ const SvgToCssPage: React.FunctionComponent = () => {
             <div className="input_field_container">
               <label>Base Font Size</label>
               <input
-                value={`${baseFont.raw_input}`}
-                name="baseFont"
+                value={`${allInputs.base.raw}`}
+                name="base"
                 type="text"
                 placeholder="16px"
-                onChange={handleInputChange}
+                onChange={changeInput}
               />
             </div>
           </div>
@@ -236,11 +205,11 @@ const SvgToCssPage: React.FunctionComponent = () => {
             <div className="input_field_container">
               <label>Custom Extension</label>
               <input
-                value={`${custom.ext}`}
+                value={`${allInputs.custom.ext}`}
                 name="customExt"
                 type="text"
                 placeholder="dvlp"
-                onChange={handleInputChange}
+                onChange={changeInput}
               />
             </div>
           </div>
