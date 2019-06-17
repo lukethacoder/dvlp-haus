db724889fd6bcbe634a28f0924a28e76f53b32c0
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2019-06-17 22:57:59 +1000

    remove html lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render () {
     return (
-      <html lang="en">
+      <html>
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="static/img/icons/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="static/img/icons/favicon-32x32.png"/>
