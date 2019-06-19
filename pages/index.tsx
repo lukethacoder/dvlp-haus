f8196cd5ef2a8211102268fd28a9e7ee07e0f612
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2019-06-20 09:46:06 +1000

    add notes markdown func / need to handle tabs in textarea

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,46 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from "react"
+import Link from "next/link"
+import Layout from "../components/Layout"
+import { NextPage } from "next"
 
 // import { loadFirebase } from '../lib/db'
 
 const IndexPage: NextPage<any> = () => {
-  // static async getInitialProps() {
-  //   console.log('hola')
-  // }
-  return (
-    <Layout title="DVLP HAUS | toolbox for developers">
-      <nav className="open">
-        <ul className="nav__list">
-          <Link href="/font-converter">
-            <li className="nav__item">
-              <h2>PX</h2>
-              <h4>Font Converter</h4>
-            </li>
-          </Link>
-          <Link href="/svg-to-css">
-            <li className="nav__item">
-              <h2>SVG</h2>
-              <h4>SVG to CSS</h4>
-            </li>
-          </Link>
-          <Link href="/useful-links">
-            <li className="nav__item">
-              <h2>USE</h2>
-              <h4>Useful Links</h4>
-            </li>
-          </Link>
-        </ul>
-      </nav>
-    </Layout>
-  )
+	// static async getInitialProps() {
+	//   console.log('hola')
+	// }
+	return (
+		<Layout title="DVLP HAUS | toolbox for developers">
+			<nav className="open">
+				<ul className="nav__list">
+					<Link href="/font-converter">
+						<li className="nav__item">
+							<h2>PX</h2>
+							<h4>Font Converter</h4>
+						</li>
+					</Link>
+					<Link href="/svg-to-css">
+						<li className="nav__item">
+							<h2>SVG</h2>
+							<h4>SVG to CSS</h4>
+						</li>
+					</Link>
+					<Link href="/notes">
+						<li className="nav__item">
+							<h2>NTS</h2>
+							<h4>Markdown Notepad</h4>
+						</li>
+					</Link>
+					<Link href="/useful-links">
+						<li className="nav__item">
+							<h2>USE</h2>
+							<h4>Useful Links</h4>
+						</li>
+					</Link>
+				</ul>
+			</nav>
+		</Layout>
+	)
 }
 
 // IndexPage.getInitialProps = async () => {
--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,46 +1,90 @@
 import * as React from "react"
+import ReactDOM from "react-dom"
+import ReactMarkdown from "react-markdown"
+
 import Link from "next/link"
 import { NextPage } from "next"
 
 import Layout from "../components/Layout"
 import Container from "../components/Container"
 
-// import { loadFirebase } from '../lib/db'
-
 const NotesPage: NextPage<any> = () => {
 	const [notesData, setNotesData] = React.useState("")
-	// static async getInitialProps() {
-	//   console.log('hola')
-	// }
+	// types 'both', 'pretty', 'both'
+	const [view, setView] = React.useState("both")
 
 	React.useEffect(() => {
-		localStorage.setItem("dvlp__haus_notes", "no notes data here")
 		const data = localStorage.getItem("dvlp__haus_notes")
-		if (data) {
+		if (!notesData && data) {
 			setNotesData(data)
+			localStorage.setItem("dvlp__haus_notes", data)
 		}
-		console.log(data)
 	})
 
-	// localStorage.setItem('_increment', JSON.stringify(this.state))
+	function updateLocalStorage(e: any) {
+		const { value } = e.target
+		localStorage.setItem("dvlp__haus_notes", value)
+		setNotesData(value)
+	}
+
+	function addTabs(e: any) {
+		e.persist()
+		// gets tab keyCode
+		if (e.keyCode === 9) {
+			e.preventDefault()
+			// add 4 spaces where current cursor is at
+			console.log(e.keyCode)
+		}
+	}
+
+	function toggleView() {
+		switch (view) {
+			case "raw":
+				setView("pretty")
+				break
+			case "pretty":
+				setView("both")
+				break
+			case "both":
+				setView("raw")
+				break
+		}
+	}
 
 	return (
 		<Layout current="Notes" title="NOTES | DVLP HAUS | toolbox for developers">
 			<Container customClass="page__notes">
-				<div className="open">
-					<ul className="nav__list"></ul>
+				<div className="page__notes_wrapper">
+					<div className="btn-toggle">
+						<p onClick={toggleView}>
+							view: <span>{view}</span>
+						</p>
+					</div>
+					<div
+						className={`notes__markdown ${
+							view === "pretty" || view === "both" ? "active" : ""
+						}`}
+					>
+						<ReactMarkdown source={notesData} />
+					</div>
+					<div
+						className={`notes__raw ${
+							view === "raw" || view === "both" ? "active" : ""
+						}`}
+					>
+						<textarea
+							value={notesData}
+							onChange={(e) => updateLocalStorage(e)}
+							onKeyDown={(e) => addTabs(e)}
+							name="svgCode"
+							placeholder="your notes will be saved - just don't hard refresh"
+							spellCheck={true}
+						></textarea>
+					</div>
 				</div>
 			</Container>
 		</Layout>
 	)
 }
-
-// IndexPage.getInitialProps = async () => {
-//   // const items: UsefulLinksProps[] = await findAll()
-//   const allCategoryOptions: any = await getAllCategories();
-//   const items: UsefulLinksProps[] = await findCategory();
-
-//   return { items, allCategoryOptions }
-// }
 
 export default NotesPage
