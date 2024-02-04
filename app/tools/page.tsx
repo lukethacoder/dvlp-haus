6bb4fd2fcfb68603cf1ecc0bdc8caa156cc51312
Luke Secomb <luke.secomb@live.com>
2024-02-04 14:57:15 +1100

    💅 home page FED (+ tools base page)

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next'
 
 import { SEO_DEFAULTS, SEO_TITLE_EXTENSION } from '@/lib/constants'
+import { AllTools } from '@/components/all-tools'
 
 export const metadata: Metadata = {
   ...SEO_DEFAULTS,
@@ -9,8 +10,12 @@ export const metadata: Metadata = {
 
 export default async function ToolsPage() {
   return (
-    <div className='flex items-center justify-center h-full'>
-      <h2>Select a tool</h2>
-    </div>
+    <section className='border-t w-full pt-6 px-4'>
+      <span className='w-full flex flex-col max-w-5xl mx-auto'>
+        <h3 className='text-3xl font-medium mb-8'>Select a tool</h3>
+
+        <AllTools />
+      </span>
+    </section>
   )
 }
---
+++ components/all-tools.tsx
@@ -0,0 +1,42 @@
+import { CATEGORIES, getToolsByCategory } from '@/lib/tools'
+import { getEntries } from '@/lib/ts'
+import TOOLS from '@/tools'
+import Link from 'next/link'
+
+export function AllTools() {
+  return (
+    <ul className='flex flex-col gap-8'>
+      {getEntries(getToolsByCategory(TOOLS))
+        // hide categories that have no tools
+        .filter(([_, category]) => category.length > 0)
+        .map(([key, category]) => {
+          const CategoryIcon = CATEGORIES[key].icon
+
+          return (
+            <li key={key}>
+              <h3 className='flex items-center text-xl font-medium mb-2'>
+                <CategoryIcon size={20} />
+                <span className='ml-2'>{CATEGORIES[key].name}</span>
+              </h3>
+              <ul className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+                {category.map((item) => (
+                  <Link
+                    key={item.slug}
+                    href={`/tools/${item.slug}`}
+                    className='hover-card w-full h-full rounded flex flex-col ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0'
+                  >
+                    <span className='font-medium text-lg'>{item.name}</span>
+                    <span className='text-muted-foreground'>
+                      {item.description}
+                    </span>
+                    <span className='hover-block'></span>
+                  </Link>
+                ))}
+              </ul>
+            </li>
+          )
+        })}
+      <li></li>
+    </ul>
+  )
+}
