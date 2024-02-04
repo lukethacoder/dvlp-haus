451f02c7a18158c1c5fc4dfe975bb8543baf3804
Luke Secomb <luke.secomb@live.com>
2024-02-05 00:07:51 +1100

    🎯 add redirects from old site tool urls

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // re-enable for static export
-  // output: 'export',
+  async redirects() {
+    return [
+      {
+        source: '/font-converter',
+        destination: '/tools/font-converter',
+        permanent: true,
+      },
+      {
+        source: '/svg-to-css',
+        destination: '/tools/svg-to-base64',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
