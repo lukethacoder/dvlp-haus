dd8b2e7cfb552219e2817988063379dabe285414
Luke Secomb <luke.secomb@live.com>
2024-02-06 19:07:32 +1100

    ⚙️ set proper metadataBase url cause VERCEL_URL doesn't want to play nice

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,5 +6,5 @@ export const SEO_DEFAULTS: Metadata = {
   title: `dvlp.haus - Toolbox for Developers`,
   description:
     'dvlp.haus (Develop House) offers an array of handy tools from conversions, pickers, calculators, formatters and more. Essential developer tools to make your life easier. ',
-  metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
+  metadataBase: new URL(`https://dvlp.haus`),
 }
